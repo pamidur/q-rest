e3cf6e9411304fae991bdfc3f6c3b107fa742a69
Kyrylo Lehonkov <klehonkov@globaltechincubator.com.au>
2018-02-21 17:24:33 +0200

    added compare ops

---
+++ QueryableRest/src/QRest.Core/Operations/Boolean/LessThanOperation.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace QRest.Core.Operations.Boolean
+{
+    public class LessThanOperation : CompareOperationBase
+    {
+        public override bool SupportsCall => true;
+
+        protected override Expression PickExpression(Expression a, Expression b)
+        {
+            return Expression.LessThan(a, b);
+        }
+    }
+}
