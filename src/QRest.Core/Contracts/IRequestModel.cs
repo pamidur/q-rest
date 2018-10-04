4f0422bd756d4346c4fce0810720a98a334697c5
Oleksandr Hulyi <agulyj@live.com>
2018-10-04 13:58:25 +0300

    made generic request model

---
+++ src/QRest.Core/Contracts/IRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace QRest.Core.Contracts
+{
+    public interface IRequestModel
+    {
+        string ModelName { get; }
+        ReadOnlyMemory<string> GetNamedQueryPart(string name);
+        Stream GetBody();
+    }
+}
