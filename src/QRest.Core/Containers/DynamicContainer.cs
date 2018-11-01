66f64b376dc8297487e9802b5ed8464a71848e18
Oleksandr Hulyi <agulyj@live.com>
2018-11-01 17:20:53 +0200

    improved dynamic container speed

--- src/QRest.Core/Containers/DynamicContainer.cs
+++ src/QRest.Core/Containers/DynamicContainer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -9,36 +8,30 @@ namespace QRest.Core.Containers
 {
     internal class DynamicContainer : DynamicObject
     {
-        public static readonly Type Type = typeof(DynamicContainer);
+        private static readonly Type _type = typeof(DynamicContainer);
+        private static readonly Type _valueContainerType = typeof(PropertyContainer);
+        private static readonly Type _valueType = typeof(object);
 
-        private readonly static Type _valueContainerType = typeof(PropertyContainer);
+        private static readonly MethodInfo _sourceSetter = _type.GetProperty(nameof(Source), typeof(PropertyContainer[])).SetMethod;
+        private static readonly MemberInfo _nameSetter = _valueContainerType.GetField(nameof(PropertyContainer.N));
+        private static readonly MemberInfo _valueSetter = _valueContainerType.GetField(nameof(PropertyContainer.V));
 
-        private IDictionary<string, object> _props = new Dictionary<string, object>();
-
-        private static readonly MethodInfo _sourceSetter = Type.GetProperty(nameof(Source), typeof(PropertyContainer[])).SetMethod;
-        private static readonly MethodInfo _nameSetter = _valueContainerType.GetProperty(nameof(PropertyContainer.N)).SetMethod;
-        private static readonly MethodInfo _valueSetter = _valueContainerType.GetProperty(nameof(PropertyContainer.V)).SetMethod;
-               
-
+#pragma warning disable CS0649 
         public class PropertyContainer
         {
-            public string N { get; set; }
-            public object V { get; set; }
+            public string N;
+            public object V;
         }
+#pragma warning restore CS0649
 
-        public DynamicContainer()
-        {
-
-        }
+        private IDictionary<string, object> _props = new Dictionary<string, object>();
 
         public PropertyContainer[] Source
         {
             set
             {
                 foreach (var prop in value)
-                {
                     _props.Add(prop.N, prop.V);
-                }
             }
         }
 
@@ -59,7 +52,6 @@ namespace QRest.Core.Containers
                 // todo:: check if need throw exception
                 return base.TryGetMember(binder, out result);
             }
-
         }
 
         public override bool TrySetMember(SetMemberBinder binder, object value)
@@ -72,24 +64,27 @@ namespace QRest.Core.Containers
             var initializers = new List<Expression>();
 
             foreach (var property in properties)
-            {
-                initializers.Add(Expression.MemberInit(Expression.New(typeof(PropertyContainer)), Expression.Bind(_valueSetter, Expression.Convert(property.Value,typeof(object))), Expression.Bind(_nameSetter, Expression.Constant(property.Key))));
-            }
+                initializers.Add(Expression.MemberInit(Expression.New(typeof(PropertyContainer)),
+                    Expression.Bind(_valueSetter, Expression.Convert(property.Value, typeof(object))),
+                    Expression.Bind(_nameSetter, Expression.Constant(property.Key))));
 
-            var createContainer = Expression.MemberInit(Expression.New(Type),Expression.Bind(_sourceSetter, Expression.NewArrayInit(_valueContainerType, initializers)));
+            var createContainer = Expression.MemberInit(Expression.New(_type),
+                Expression.Bind(_sourceSetter, Expression.NewArrayInit(_valueContainerType, initializers)));
             return createContainer;
         }
 
         public static Expression CreateReadProperty(Expression context, string name)
         {
-            var ed = Expression.Dynamic(Microsoft.CSharp.RuntimeBinder.Binder.GetMember(0, name, context.Type, new[] { Microsoft.CSharp.RuntimeBinder.CSharpArgumentInfo.Create(0, null) }), typeof(object), context);
+            var ed = Expression.Dynamic(
+                Microsoft.CSharp.RuntimeBinder.Binder.GetMember(0, name, context.Type,
+                new[] { Microsoft.CSharp.RuntimeBinder.CSharpArgumentInfo.Create(0, null) }), typeof(object), context);
 
             return ed;
         }
 
         public static bool IsContainerType(Type type)
         {
-            return type == Type;
+            return type == _type;
         }
     }
 }
