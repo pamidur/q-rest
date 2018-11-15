3214efdb7aa7613a776073cb3a4dc0da5861e272
Oleksandr Hulyi <agulyj@live.com>
2018-11-15 11:26:32 +0200

    test refactoring

--- test/QRest.Compiler.Standard.Tests/CompileOperationsTests.cs
+++ test/QRest.Compiler.Standard.Tests/CompileOperationsTests.cs
@@ -8,18 +8,6 @@ using Xunit;
 
 namespace QRest.Compiler.Standard.Tests
 {
-    public class CompileTestSubClass
-    {
-        public int Id { get; set; }
-    }
-    public class CompileTestClass
-    {
-        public int IntProperty { get; set; }
-        public string StringProperty { get; set; }
-        public DateTime DateTimeProperty { get; set; }
-        public CompileTestSubClass SubProperty { get; set; }
-    }
-
     public class CompileOperationsTests
     {
         private readonly StandardCompiler _compiler;
---
+++ test/QRest.Compiler.Standard.Tests/TestData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QRest.Compiler.Standard.Tests
+{
+    public class CompileTestSubClass
+    {
+        public int Id { get; set; }
+    }
+    public class CompileTestClass
+    {
+        public int IntProperty { get; set; }
+        public string StringProperty { get; set; }
+        public DateTime DateTimeProperty { get; set; }
+        public CompileTestSubClass SubProperty { get; set; }
+    }
+}
