d9c707c8036c6c50c542ef8c34316ba86f4a5b70
Oleksandr Hulyi <agulyj@live.com>
2019-01-08 15:13:24 +0200

    added any operation

---
+++ src/QRest.Core/Operations/Aggregations/AnyOperation.cs
@@ -0,0 +1,37 @@
+﻿using QRest.Core.Contracts;
+using QRest.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace QRest.Core.Operations.Aggregations
+{
+    public class AnyOperation : OperationBase
+    {
+        public override string Key { get; } = "any";
+
+        public override Expression CreateExpression(ParameterExpression root, Expression context, IReadOnlyList<Expression> arguments, IAssemblerContext assembler)
+        {
+            var args = new List<Expression>
+            {
+                context
+            };
+
+            if(arguments.Count==1)
+            {
+                if (arguments[0].NodeType != ExpressionType.Lambda)
+                    throw new ExpressionCreationException();
+
+                args.Add(arguments[0]);
+            }
+
+            if (arguments.Count > 1)
+                throw new ExpressionCreationException();
+
+            var exp = Expression.Call(typeof(Queryable), nameof(Queryable.Any), new Type[] { context.GetQueryElementType() }, args.ToArray());
+
+            return exp;
+        }      
+    }
+}
